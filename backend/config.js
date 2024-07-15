69e3e68fdeb0d2d08be085279022e7aa21cda2d1
Ethan Walker <81318227+EthanWalkerGit@users.noreply.github.com>
2024-07-15 09:59:34 -0400

    relative url

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   mongoURI: 'mongodb+srv://admin:vQLR9HlZZEpc9DCV@drdisccluster.v8dront.mongodb.net/?retryWrites=true&w=majority&appName=DrDiscCluster',
   jwtSecret: 'hello',
-  backendURL: 'https://drdiscmarket.ca'
 };
