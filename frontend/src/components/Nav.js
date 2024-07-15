99500bf2515633124bcbed9294a4b0399bd5f1d3
Ethan Walker <81318227+EthanWalkerGit@users.noreply.github.com>
2024-07-15 14:45:56 -0400

    new ham menu

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,12 +1,26 @@
+import React, { useState } from 'react';
 import { navLinks } from "../constants";
 import { FiAlignJustify, FiUser, FiShoppingCart, FiSearch } from "react-icons/fi";
 import { IconContext } from "react-icons";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+
+  const [showNavLinks, setShowNavLinks] = useState(false);
+
+  const toggleNavLinks = () => {
+    setShowNavLinks(!showNavLinks);
+  };
+
+
   return (
       <header className="py-2 absolute z-10 w-full bg-black">
         <nav className="flex h-16 justify-between items-center max-container">
+        <div className='block lg:hidden pl-10 hover:cursor-pointer' onClick={toggleNavLinks}>
+            <IconContext.Provider value={{ color: "white", size: "35px" }}>
+              <FiAlignJustify />
+            </IconContext.Provider>
+          </div>
         <div className='hidden sm:block w-nav-i-width lr-12'></div>
           <ul className='flex-1 flex justify-center items-center gap-10 max-lg:hidden'>
             {navLinks.map((item) => (
@@ -17,9 +31,9 @@ const Nav = () => {
               </li>
             ))}
           </ul>
-          <div className='hidden lg:block w-nav-i-width pr-12'>
+          <div className='hidden lg:block w-nav-i-width'>
             <IconContext.Provider value={{ color: "white", size: "25px" }}>
-              <ul className="flex flex-row justify-between items-center">
+              <ul className="flex flex-row justify-evenly items-center">
                 <li className="hover:cursor-pointer"><FiSearch /></li> 
                 <li className="hover:cursor-pointer"><FiShoppingCart /></li> 
                 <li className="hover:cursor-pointer">
@@ -30,12 +44,16 @@ const Nav = () => {
               </ul>
             </IconContext.Provider>
           </div>
-          <div className='block lg:hidden pr-10 hover:cursor-pointer'>
-            <IconContext.Provider value={{ color: "white", size: "35px" }}>
-              <FiAlignJustify />
-            </IconContext.Provider>
-          </div>
         </nav>
+          {showNavLinks && (<ul className="flex flex-col lg:hidden transition-opacity delay-1000 ease-in-out">
+            {navLinks.map((item, index) => (
+              <li key={item.label} className={`py-3 pl-11 ${index % 2 === 0 ? 'bg-listing' : ''}`}>
+                <Link to={item.href} className='leading-normal text-sm text-white font-merriweather'>
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>)}
       </header>
   )
 }
